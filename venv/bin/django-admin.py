e98b3dd00b971e49b5dbfedfd46cb64c98fe21a9
Frederico Maia Arantes <fredericomaia10@gmail.com>
2020-05-13 19:22:41 +0100

    Added flake8 and Travis CI

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/fredericomaiaarantes/dev/projects/django/recipe-app-api/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
